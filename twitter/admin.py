4e9ac35601c1d7da2284273bdb3d057f95c9118d
Rohit Kumar <rgrohit81@gmail.com>
2020-03-12 01:43:01 +0530

    removed other model except tweet

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import Tweet, Like, Comment
+from .models import Tweet
 
 admin.site.register(Tweet)
-admin.site.register(Like)
-admin.site.register(Comment)
